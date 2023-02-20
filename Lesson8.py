c8a5ea89619d12337e3320294dd6bdd9fb0cd874
DevCok22 <brownar@cokcu.com>
2023-02-20 14:08:47 -0500

    Python lesson added

---
+++ Lesson8.py
@@ -0,0 +1,10 @@
+# Function Parameters
+
+def greet(_name):
+    print(f"Hello comrade {_name}")
+    print("How are you doing")
+    print("How is the weather comrade.")
+
+
+name = input("Please enter your name: ")
+greet(name)
