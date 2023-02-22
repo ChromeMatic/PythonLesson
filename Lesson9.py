7ffbaf6a01cdf9ca51124fa318f6cd596027899f
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2023-02-22 14:57:43 -0500

    Learning Python Dictionary  - Nested Dictionary with list  - Nested Dictionary with Dictionary  - Dictionary operation

--- Lesson9.py
+++ Lesson9.py
@@ -18,8 +18,48 @@ empty_dictionary = {}
 # Wipe an existing dictionary
 # programming_dictionary = {}
 # print(programming_dictionary)
+
+# Edit an element in dictionary
 programming_dictionary["Bug"] = "A moth inside of your computer"
 
 # Get elements form dictionary
 for key in programming_dictionary:
     print(f" {key}: {programming_dictionary[key]}")
+
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nesting a List in a dictionary
+travel_log1 = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+# Nesting a dictionary in a dictionary
+travel_log2 = {
+    "France": {
+      "cities_visited": ["Paris", "Lille", "Dijon"],
+      "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+}
+
+# Nesting a Dictionary in a list
+travel_log_arr = [
+    {
+      "country": "France",
+      "cities_visited": ["Paris", "Lille", "Dijon"],
+      "total_visits": 12
+    },
+    {
+      "country": "Germany",
+      "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+      "total_visits": 3
+    }
+]
