bcb2a6daab01c085971c6060842280c378b8029b
DevCok22 <brownar@cokcu.com>
2023-02-23 16:17:36 -0500

    OPP added

---
+++ OOP_In_Python.py
@@ -0,0 +1,19 @@
+# (OOP)
+# Object-Oriented Programming In Python
+from turtle import Turtle, Screen
+
+Object = Turtle()
+Object.shape("turtle")
+Object.color("green")
+
+Object.forward(100)
+Object.left(90)
+Object.forward(100)
+Object.left(90)
+Object.forward(100)
+Object.left(90)
+Object.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
