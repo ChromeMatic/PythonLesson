8c53f8ec43d8d11860d7a7cfe60876a5f1f0d7e6
DevCok22 <brownar@cokcu.com>
2023-03-01 09:47:47 -0500

    Changes added

--- OOP_In_Python.py
+++ OOP_In_Python.py
@@ -1,19 +1,32 @@
 # (OOP)
 # Object-Oriented Programming In Python
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# Object = Turtle()
+# Object.shape("turtle")
+# Object.color("gold")
+#
+# Object.forward(100)
+# Object.left(90)
+# Object.forward(100)
+# Object.left(90)
+# Object.forward(100)
+# Object.left(90)
+# Object.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-Object = Turtle()
-Object.shape("turtle")
-Object.color("green")
+from prettytable import PrettyTable
 
-Object.forward(100)
-Object.left(90)
-Object.forward(100)
-Object.left(90)
-Object.forward(100)
-Object.left(90)
-Object.forward(100)
+names = ["Pikachu", "Squirtle", "Charmander"]
+Types = ["Electric", "Water", "Fire"]
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+
+table.add_column("Pokemon Name", names)
+table.add_column("Type", Types)
+
+print(table)
+
